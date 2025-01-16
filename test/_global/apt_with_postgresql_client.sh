fc231248a34410d8f04e7b573781864b254ecb03
Viktor Schmidt <viktorianer4life@gmail.com>
2025-01-16 14:02:19 +0100

    Feature Aptfile Dependencies (#1)
    * Add apt feature for managing Aptfile dependencies
    
    * Update README
    
    * Remove example `color` and `hello` features.
    
    * Fix global test

--- test/_global/apt_with_postgresql_client.sh
+++ test/_global/apt_with_postgresql_client.sh
@@ -3,11 +3,11 @@
 # The 'test/_global' folder is a special test folder that is not tied to a single feature.
 #
 # This test file is executed against a running container constructed
-# from the value of 'color_and_hello' in the tests/_global/scenarios.json file.
+# from the value of 'all_features' in the tests/_global/scenarios.json file.
 #
 # The value of a scenarios element is any properties available in the 'devcontainer.json'.
 # Scenarios are useful for testing specific options in a feature, or to test a combination of features.
-# 
+#
 # This test can be run with the following command (from the root of this repo)
 #    devcontainer features test --global-scenarios-only .
 
@@ -16,17 +16,13 @@ set -e
 # Optional: Import test library bundled with the devcontainer CLI
 source dev-container-features-test-lib
 
-echo -e "The result of the 'color' command will be:\n"
-color
-echo -e "The result of the 'hello' command will be:\n"
-hello
-echo -e "\n"
-
 # Feature-specific tests
 # The 'check' command comes from the dev-container-features-test-lib.
-check "check purple is my favorite color" bash -c "color | grep 'my favorite color is purple'"
-check "check I am greeting with 'Greetings'" bash -c "hello | grep 'Greetings, $(whoami)'"
+# Check if libpq-dev is installed
+check "libpq-dev installed" bash -c "dpkg -l | grep libpq-dev"
 
+# Check if PostgreSQL client (version 17) is installed
+check "PostgreSQL client version 17 installed" bash -c "psql --version | grep -E 'PostgreSQL.*17(\.[0-9]+)?'"
 
 # Report result
 # If any of the checks above exited with a non-zero exit code, the test will fail.
